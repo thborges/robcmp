44d4d3a5e8196707a99e74738a7b542efccc57ac
Vitor Subhi <vitorsubhi@gmail.com>
2019-11-20 22:58:57 -0300

    Some fixes at FunctionCall

--- src/FunctionCall.cpp
+++ src/FunctionCall.cpp
@@ -2,6 +2,7 @@
 
 Value *FunctionCall::generate(Function *func, BasicBlock *block, BasicBlock *allocblock) {
 	Function *cfunc = (Function*)search_symbol(name, mainblock);
+
 	if (cfunc == NULL) {
 		yyerrorcpp("Function " + name + " not defined.");
 		return NULL;
@@ -9,8 +10,8 @@ Value *FunctionCall::generate(Function *func, BasicBlock *block, BasicBlock *all
 
 	vector<Value*> args;	
 	for (int i = 0; i < parameters->getNumParams(); i++){
-//		args.push_back(parameters->getParamElement(i));
-		args.push_back(search_symbol("a"));
+		Value *valor = parameters->getParamElement(i)->generate(func, block, allocblock);
+		args.push_back(valor);
 	}
 	ArrayRef<Value*> argsRef(args);
 	return CallInst::Create(cfunc, argsRef, "", block);
